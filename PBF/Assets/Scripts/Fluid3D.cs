125f49ed903dc60dcc517b25755856ab9917668d
soma-yokota <soma.yokota@fj.ics.keio.ac.jp>
2023-04-10 22:16:13 +0900

    first comit

---
+++ PBF/Assets/Scripts/Fluid3D.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace Yokota.Fluid
+{
+    public struct FluidParticle
+    {
+        public Vector3 Position;
+        public Vector3 Velocity;
+    }
+
+
+    public class Fluid3D : FluidBase<FluidParticle>
+    {
+        [SerializeField] private float ballRadius = 0.1f;           // 粒子位置初期化時の球半径．
+        [SerializeField] private float MouseInteractionRadius = 1f; // マウスインタラクションの範囲の広さ．
+
+        private bool isMouseDown;
+        private Vector3 screenToWorldPointPos;
+
+        /// <summary>
+        /// パーティクル初期位置の設定
+        /// </summary>
+        /// <param name="particles"></param>
+        protected override void InitParticleData(ref FluidParticle[] particles)
+        {
+            for(int i = 0; i < NumParticles; i++)
+            {
+                particles[i].Velocity = Vector3.zero;
+                particles[i].Position = range / 2f + Random.insideUnitSphere * ballRadius;  // 球形に粒子を初期化する．
+            }
+        }
+
+        /// <summary>
+        /// ComputeShader の定数バッファに追加する
+        /// </summary>
+        /// <param name="cs"></param>
+        protected override void AdditionalCSParams(ComputeShader cs)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                isMouseDown = true;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                isMouseDown = false;
+            }
+
+            if (isMouseDown)
+            {
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.z = 10f;
+                screenToWorldPointPos = Camera.main.ScreenToWorldPoint(mousePos);
+            }
+
+            cs.SetVector("_MousePos", screenToWorldPointPos);
+            cs.SetFloat("_MouseRadius", MouseInteractionRadius);
+            cs.SetBool("_MouseDown", isMouseDown);
+        }
+
+    }
+}
